3ca451c907e833f25769509734f69491e5d32e4d
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-10-16 16:19:54 +0300

    change whatsapp icon dir

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 
 export default function Whatsapp() {
+    const {i18n} = useTranslation()
     return (
-        <div className='fixed cursor-pointer  z-30 shlg bottom-6 right-3 bg-green-500 p-3 rounded-full'>
+        <div className={`fixed cursor-pointer  z-30 shlg bottom-6 ${i18n.language === "ar" ? 'left-3' :'right-3'} bg-green-500 p-3 rounded-full`}>
             <Link to={'https://wa.me/201226621763'} target='_blank'>
                 <i className='fa-brands fa-whatsapp text-3xl text-white'></i>
             </Link>
