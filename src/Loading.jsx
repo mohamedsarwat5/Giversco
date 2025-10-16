676afa9f94000ed96dac72365820b5d8981dc2b9
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-10-16 16:46:15 +0300

    fix loading ar

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -1,41 +1,23 @@
 import { motion } from "framer-motion"
 import { span } from "framer-motion/client"
-import { useEffect, useState } from "react"
 import { useTranslation } from "react-i18next"
 
 export default function Loading() {
 
-    const { t, i18n } = useTranslation()
+    const { t } = useTranslation()
+    const text = t('logo');
+    const isArabic = /[\u0600-\u06FF]/.test(text); // detect Arabic letters
+
+
     return (
 
         <div className="fixed inset-0  bg-green flex items-center justify-center text-center text-white text-6xl">
             <div class="loading loading01">
-
-
-                {t(`logo`).split("").map((l, i) => (
-                    <span key={i}>{l}</span>
-                ))}
-                {/* <span>L</span>
-                <span>O</span>
-                <span>A</span>
-                <span>D</span>
-                <span>I</span>
-                <span>N</span>
-                <span>G</span> */}
+                {isArabic
+                    ? <span>{text}</span>
+                    : text.split("").map((l, i) => <span key={i}>{l}</span>)
+                }
             </div>
-
-            {/* <h2 className="animate-bounce"> </h2> */}
-            {/* {t(`logo`).split("").map((letter, i, arr) => (
-                <motion.h2
-                    className="block"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: [1, 0, 1] }}
-                    transition={{ duration: 1.5, delay: i * 0.1, repeat: Infinity, repeatType: 'loop', ease: "easeInOut", }}
-
-                    key={i}> {letter}</motion.h2>
-            ))
-            } */}
-
         </div >
     )
 }
